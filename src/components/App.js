51b43c0a7d50717d6edfe2469555d8f145a7a200
GStewartN <stewart.nathang@gmail.com>
2018-01-25 13:54:47 -0800

    Change file structure, move App to components folder

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <h2>Reusable React Components</h2>
+        <p>React components for plug and play use in other apps.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
