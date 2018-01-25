34b828dd7061e8663262189c84dc22c89c171676
GStewartN <stewart.nathang@gmail.com>
2018-01-25 14:37:09 -0800

    Create Header with children prop, pass jsx through App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import './App.css';
+import Header from './Header';
 
 const App = () => {
   return (
     <div>
-      <header className="App-header">
-        <h2>Reusable React Components</h2>
-        <p>React components for plug and play use in other apps.</p>
-      </header>
+      <Header>
+        <div id="Header">
+          <h2>Reusable React Components</h2>
+          <p>This simple header uses the children prop, with all jsx and style rules passed from the parent.</p>
+        </div>
+      </Header>
     </div>
   );
 }
